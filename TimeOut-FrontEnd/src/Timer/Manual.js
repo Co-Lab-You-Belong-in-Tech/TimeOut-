971f7cc44765f1eef4ed51c6f0572c298c91e885
Mohammed Naeem <mohammednaeem345@gmail.com>
2024-01-06 01:38:58 -0500

    splash

--- TimeOut-FrontEnd/src/Timer/Manual.js
+++ TimeOut-FrontEnd/src/Timer/Manual.js
@@ -5,57 +5,14 @@ import { DatePickerOptions } from '@react-native-community/datetimepicker';
 import { View, Button, DateTimePicker, Modal } from 'react-native';
 
 const Manual = () => {
-  const [selectedDate, setSelectedDate] = useState(new Date());
-  const [selectedTime, setSelectedTime] = useState(new Date());
-  const [isDatePickerVisible, setDatePickerVisible] = useState(false);
-  const [isTimePickerVisible, setTimePickerVisible] = useState(false);
-
-  const showDatePicker = () => {
-    setDatePickerVisible(true);
-  };
-
-  const hideDatePicker = () => {
-    setDatePickerVisible(false);
-  };
-
-  const handleDateChange = (date) => {
-    setSelectedDate(date);
-    hideDatePicker();
-  };
-
-  const showTimePicker = () => {
-    setTimePickerVisible(true);
-  };
-
-  const hideTimePicker = () => {
-    setTimePickerVisible(false);
-  };
-
-  const handleTimeChange = (time) => {
-    setSelectedTime(time);
-    hideTimePicker();
-  };
+  
 
   return (
+    <SafeAreaView>
     <View>
-      <Button title="Select Date" onPress={showDatePicker} />
-      <Text>Selected Date: {selectedDate.toDateString()}</Text>
-      <Modal visible={isDatePickerVisible} animationType="slide">
-        <View>
-          <DatePickerOptions date={selectedDate} onDateChange={handleDateChange} />
-          <Button title="Done" onPress={hideDatePicker} />
-        </View>
-      </Modal>
-
-      <Button title="Select Time" onPress={showTimePicker} />
-      <Text>Selected Time: {selectedTime.toLocaleTimeString()}</Text>
-      <Modal visible={isTimePickerVisible} animationType="slide">
-        <View>
-          <DateTimePicker date={selectedTime} onDateChange={handleTimeChange} />
-          <Button title="Done" onPress={hideTimePicker} />
-        </View>
-      </Modal>
+      <Text>Manual Time Page</Text>
     </View>
+    </SafeAreaView>
   );
 };
 
